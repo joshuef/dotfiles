03af64d4d38941fdbf73ad99f10c6eafa9e9eb55
Josh Wilson <joshuef@gmail.com>
2019-08-31 18:44:38 +0200

    ignore some atom, add more ssh

--- get_secrets.sh
+++ get_secrets.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "=>>>Pulling secrets"
 
-op get document 'github_rsa' > github_rsa --tags devops
+op get document 'zhwkovejeqq7x55sy3ba6ovy5i' > github_rsa --tags devops
+op get document 'op get document kk2wcb4oiptf7g2zprmdgo34uy' > sharing_rsa --tags devops
 # op get document 'zsh_private' > zsh_private
 # op get document 'zsh_history' > zsh_history
 
 rm -f ~/.ssh/github_rsa
+rm -f ~/.ssh/sharing_rsa
 ln -sfn $(pwd)/github_rsa ~/.ssh/github_rsa
+ln -sfn $(pwd)/sharing ~/.ssh/sharing
 chmod 0600 ~/.ssh/github_rsa
+chmod 0600 ~/.ssh/sharing_rsa
+ssh-keygen -y -f ~/.ssh/sharing_rsa > ~/.ssh/sharing_rsa.pub
 
 # ln -sfn $(pwd)/zsh_private ~/.zsh_private
 # ln -sfn $(pwd)/zsh_history ~/.zsh_history
