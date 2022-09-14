0407208b7983a21148ce691e34c9f203f1b16af6
Josh Wilson <joshuef@gmail.com>
2022-09-14 17:26:44 +0200

    morerg

--- get_secrets.sh
+++ get_secrets.sh
@@ -2,8 +2,8 @@
 echo "=>>>Pulling secrets"
 
 
-op get document 'zhwkovejeqq7x55sy3ba6ovy5i' > github_rsa
-op get document 'kk2wcb4oiptf7g2zprmdgo34uy' > sharing_rsa
+# op get document 'zhwkovejeqq7x55sy3ba6ovy5i' > github_rsa
+# op get document 'kk2wcb4oiptf7g2zprmdgo34uy' > sharing_rsa
 
 
 
