abc7999a6001403ce7917a0b9785c36d07e05ff9
Josh Wilson <joshuef@gmail.com>
2023-11-15 20:42:25 +0100

    moreif

--- setup23.sh
+++ setup23.sh
@@ -5,14 +5,15 @@
 # only install if not already installed
 if [ ! -d "/nix" ]; then
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+  mv /etc/nix/nix.conf /etc/nix/nix.conf.before.build.bkp
+  echo "nix basic installed"
+  echo "reloading the shell. run this cmd again afterwards"
+
+  # reloading the shell now
+  zsh
 fi
 
-echo "nix basic installed"
 
-# reloading the shell now
-zsh
-
-mv /etc/nix/nix.conf /etc/nix/nix.conf.before.build.bkp
 # and building with nix
 nix/bin/build
 
